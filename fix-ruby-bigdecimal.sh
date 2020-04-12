6309221d32b7d97d76b2781b43e74fb75caad068
AbdulkaderAlrazj <34913203+AbdulkaderAlrazj@users.noreply.github.com>
2020-04-12 19:05:31 +0300

    Update fix-ruby-bigdecimal.sh

--- fix-ruby-bigdecimal.sh
+++ fix-ruby-bigdecimal.sh
@@ -3,8 +3,8 @@
 ##  Fix linking error for Ruby Bigdecimal
 ##  native extensions.
 ##
-for i in aarch64-linux-android arm-linux-androideabi \
-    i686-linux-android x86_64-linux-android; do
+#for i in aarch64-linux-android arm-linux-androideabi \
+ #   i686-linux-android x86_64-linux-android; do
 
     if [ -e "$PREFIX/lib/ruby/2.6.0/${i}/bigdecimal.so" ]; then
         if [ -n "$(patchelf --print-needed "$PREFIX/lib/ruby/2.6.0/${i}/bigdecimal/util.so" | grep bigdecimal.so)" ]; then
